efc7ca47f9f4e4bad867459e844c2d6669d20a6b
williamshty <williamshty@gmail.com>
2018-03-30 17:59:47 +0800

    added navigation bar styling

--- src/components/Authority/AllArtworksDisplay.vue
+++ src/components/Authority/AllArtworksDisplay.vue
@@ -1,37 +1,116 @@
 <template>
   <div class="">
-      <div class="row">
-        <div class="col-2-of-12">
-          <p class="new" style="background-color:red">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, quam dolore! Porro veritatis modi quos, in dolores odio illum odit officiis sunt rem aliquid qui esse minima iste itaque autem?
-          </p>
+    <div class="navigation navigation--authority">
+      Authority Portal
+    </div>
+    <el-row :gutter="20">
+      <el-col :span="4">
+        <div class="grid-content">
+          <el-button class="btn-add" type="primary" icon="el-icon-plus">Add New Artwork</el-button>
         </div>
-        <div class="col-8-of-12">
-           <p class="new" style="background-color:red">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, quam dolore! Porro veritatis modi quos, in dolores odio illum odit officiis sunt rem aliquid qui esse minima iste itaque autem?
-          </p>
+      </el-col>
+      <el-col :span="16">
+        <div class="grid-content">
+          <el-table :data="tableData" stripe style="width: 100%">
+            <el-table-column prop="artworkID" label="Artwork ID">
+            </el-table-column>
+            <el-table-column prop="artworkTitle" label="Artwork Title">
+            </el-table-column>
+            <el-table-column prop="artworkArtist" label="Artist">
+            </el-table-column>
+            <el-table-column prop="artworkOwner" label="Owner">
+            </el-table-column>
+            <el-table-column label="Documents">
+              <template slot-scope="scope">
+                <el-button
+                size="mini"
+                type="danger"
+                @click="viewDocuments(scope.row.artworkID)">
+                View Documents</el-button>
+              </template>
+            </el-table-column>
+            <el-table-column label="History">
+              <template slot-scope="scope">
+                <el-button
+                size="mini"
+                type="warning"
+                @click="viewHistory(scope.row.artworkID)">
+                View History</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </div>
-        <div class="col-2-of-12">
-           <p class="new" style="background-color:red">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, quam dolore! Porro veritatis modi quos, in dolores odio illum odit officiis sunt rem aliquid qui esse minima iste itaque autem?
-          </p>
-        </div>
-      </div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content"></div>
+      </el-col>
+    </el-row>
   </div>
 </template>
-
 <script>
 export default {
   data () {
     return {
+      tableData: [
+        {
+          artworkID: '123',
+          artworkTitle: 'Sunflower',
+          artworkArtist: 'Van Goh',
+          artworkOwner: 'STY'
+        },
+        {
+          artworkID: '223',
+          artworkTitle: 'Sunflower',
+          artworkArtist: 'Van Goh',
+          artworkOwner: 'STY'
+        },
+        {
+          artworkID: '333',
+          artworkTitle: 'Sunflower',
+          artworkArtist: 'Van Goh',
+          artworkOwner: 'STY'
+        },
+        {
+          artworkID: '444',
+          artworkTitle: 'Sunflower',
+          artworkArtist: 'Van Goh',
+          artworkOwner: 'STY'
+        }
+      ]
     }
   },
   methods: {
+    viewDocuments (artworkID) {
+      console.log(`Documents for ID: ${artworkID}`)
+    },
+    viewHistory (artworkID) {
+      console.log(`History for ID: ${artworkID}`)
+    },
+    loadAllArtworks () {
+      console.log(`Loading All Artworks`)
+    }
 
   },
   beforeMount () {
+    this.loadAllArtworks()
   }
 }
 </script>
-<style>
+<style scoped>
+.el-row {
+    margin-bottom: 20px;
+
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .btn-add {
+    position: relative;
+    left: 50%;
+    transform: translate(-50%,50%)
+  }
 </style>
